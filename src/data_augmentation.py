16bdc2dfbbe2a3efc2a8c9069ffaacd0c4759feb
AhmadAny <49196123+AmadAny@users.noreply.github.com>
2026-05-14 19:38:57 +0300

    Update data_augmentation.py

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -1,18 +1,5 @@
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-
-def get_train_transform(size=160):
-    return A.Compose([
-        A.Resize(size, size),
-        A.OneOf([
-            A.GaussianBlur(blur_limit=(3,7)),
-            A.MotionBlur(blur_limit=(5)),
-        ], p=0.3),
-        A.RandomBrightnessContrast(p=0.3),
-        A.HorizontalFlip(p=0.5),
-        A.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5)),
-        ToTensorV2(),
-    ])
     
 def get_val_transform(size=160):
     return A.Compose([
